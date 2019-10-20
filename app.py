01563c52c20bcd418fd6d28dc1b1c092c3b0373f
miks <mikhail12klimovich@gmail.com>
2019-10-20 17:58:26 +0300

    handle token file imports

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from forecast_bot import ForecastBot
-from secret import telegram_token
+import sys
 import time
 
+from forecast_bot import ForecastBot
+
+try:
+    from secret import telegram_token
+except ModuleNotFoundError:
+    print('Need create file "secret.py" with "telegram_token" var')
+    sys.exit(1)
 
 forecast = ForecastBot(token=telegram_token,
                        img_urls=['http://www.pogoda.by/mg/366/noaa_T26850.gif',
                                  'http://www.pogoda.by/mg/366/noaa_W26850.gif'],
                        followers='members.txt')
-
 
 if not forecast.is_available():
     print(f'No forecast, downloading - {time.ctime()}')
